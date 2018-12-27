a2b7ec1b0fc561aa7340132438f0a0f3ded8d69b
Mirren Malcolm-Neale <>
2018-12-27 17:26:05 +0000

    Adjustments to day one and adding day 2

--- Day_1/CodeAdvent_DayOne/CodeAdvent_DayOne/main.cpp
+++ Day_1/CodeAdvent_DayOne/CodeAdvent_DayOne/main.cpp
@@ -8,27 +8,62 @@
 
 #include <iostream>
 #include <fstream>
+#include <numeric>
+#include <vector>
+#include <unordered_set>
+#include <iterator>
 
 using namespace std;
+constexpr static bool part2 = true;
 
 int main(int argc, const char * argv[]) {
-    std::string str;
-    int result = 0;
     
-    ifstream in("input.txt");
-    if(!in)
-    {
-        printf("No input file found");
-        return 1;
-    }
     
-    while(std::getline(in, str))
+    ifstream inputFile("input.txt");
+    
+    int sum{0};
+    if constexpr (part2)
     {
-        result += atoi(str.c_str());
+        std::unordered_set<int>uniq;
+        std::vector<int> input;
+        std::copy(std::istream_iterator<int> (inputFile), {}, std::back_inserter(input));
+        int i {0};
         
+        //Inserts element from original input file to new unordered set only if doesn't already exist
+            //Where element = sum of sequential frequencies from input file (summing value of vector map pair)
+        while(uniq.insert (sum += input[i]).second)
+        {
+            i = (i + 1) % input.size();;
+        }
+    }
+    else
+    {
+        sum = std::accumulate(std::istream_iterator<int>(inputFile), {}, 0);
     }
     
-    std::cout << result << std::endl;
-    return 0;
+    std::cout << sum << '\n';
+    return EXIT_SUCCESS;
+    
+    
+    /*
+    int sum{0};
+    if constexpr (part2)
+    {
+        std::unordered_set<int>uniq;
+        std::vector<int> input;
+        std::copy(std::istream_iterator<int> (std::cin), {}, std::back_inserter(input));
+        int i {0};
+        while(uniq.insert (sum += input[i]).second)
+              {
+                  i =( i + 1) % input.size();;
+              }
+    }
+    else{
+            sum = std::accumulate(std::istream_iterator<int>(std::cin), {}, 0);
+        }
+    std::cout << sum << '\n';
+    return EXIT_SUCCESS;
+    */
+    
 }
 

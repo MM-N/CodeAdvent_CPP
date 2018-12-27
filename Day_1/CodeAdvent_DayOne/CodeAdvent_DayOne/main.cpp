e6b47367e5d8addbfed134640835d6379572b509
Mirren Malcolm-Neale <>
2018-12-27 18:23:34 +0000

    Tidy up and re add part 1

--- Day_1/CodeAdvent_DayOne/CodeAdvent_DayOne/main.cpp
+++ Day_1/CodeAdvent_DayOne/CodeAdvent_DayOne/main.cpp
@@ -14,56 +14,36 @@
 #include <iterator>
 
 using namespace std;
-constexpr static bool part2 = true;
+
+// Get sum of all frequencies(ints) from input text
+int SumAllFreq(vector<int> &input)
+{
+    return accumulate(input.begin(), input.end(), 0);
+}
+
+// Find first sum of all frequencies(ints) that is repeated.
+int FirstFreqSumHitTwice(vector<int> &input)
+{
+    int i{0};
+    int sum{};
+    unordered_set<int> uniqueFreq;
+    
+    // Keep looping through entire text, adding each unique freq. to list until double freq. reached. Sum will return freq. hit twice.
+    while(uniqueFreq.insert(sum += input[i]).second)
+    {
+        i = (i+1) % input.size();
+    }
+    return sum;
+}
 
 int main(int argc, const char * argv[]) {
     
-    
     ifstream inputFile("input.txt");
-    
-    int sum{0};
-    if constexpr (part2)
-    {
-        std::unordered_set<int>uniq;
-        std::vector<int> input;
-        std::copy(std::istream_iterator<int> (inputFile), {}, std::back_inserter(input));
-        int i {0};
-        
-        //Inserts element from original input file to new unordered set only if doesn't already exist
-            //Where element = sum of sequential frequencies from input file (summing value of vector map pair)
-        while(uniq.insert (sum += input[i]).second)
-        {
-            i = (i + 1) % input.size();;
-        }
-    }
-    else
-    {
-        sum = std::accumulate(std::istream_iterator<int>(inputFile), {}, 0);
-    }
-    
-    std::cout << sum << '\n';
-    return EXIT_SUCCESS;
-    
-    
-    /*
-    int sum{0};
-    if constexpr (part2)
-    {
-        std::unordered_set<int>uniq;
-        std::vector<int> input;
-        std::copy(std::istream_iterator<int> (std::cin), {}, std::back_inserter(input));
-        int i {0};
-        while(uniq.insert (sum += input[i]).second)
-              {
-                  i =( i + 1) % input.size();;
-              }
-    }
-    else{
-            sum = std::accumulate(std::istream_iterator<int>(std::cin), {}, 0);
-        }
-    std::cout << sum << '\n';
-    return EXIT_SUCCESS;
-    */
+    vector<int> input {istream_iterator<int>(inputFile), {}};
+    cout << "Sum Of All Frequencies: " << SumAllFreq(input) << endl;;
+    cout << "First Sum of Freq Reached Twice: " << FirstFreqSumHitTwice(input) << endl;
+    return 0;
     
 }
+
 
